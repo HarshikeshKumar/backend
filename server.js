7d1a9a76f7c107fb83ccaf67c0ed6975cdebfd3f
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-03-07 02:58:19 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+app.use(cors());
+
+app.get("/api/greet", (req, res) => {
+  const name = req.query.name;
+  if (!name) {
+    return res.status(400).json({ error: "Name is required." });
+  }
+  res.json({ message: `Hello, ${name}! Welcome to Younglabs.` });
+});
+
+app.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`)
+);

b94d09e92cb4d2c0692dd4febb4aad385dfc7938
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-03-07 11:39:25 +0530

    Chenging for deployement

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 app.use(cors());
 
 app.get("/api/greet", (req, res) => {
